e2c799889e7dc53504fdc460b129bf6c57f1f850
m-swelam <mid2o0o11@gmail.com>
2021-11-09 13:59:26 +0200

    section 5 - 31

--- section-5/app.js
+++ section-5/app.js
@@ -3,8 +3,24 @@ const app = express();
 
 const port = process.env.PORT || 9999;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.send('<h1>home</h1>')
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <h1>home</h1>
+  </body>
+  </html>
+  `);
 });
 
 app.get("/post/:id", (req, res) => {
