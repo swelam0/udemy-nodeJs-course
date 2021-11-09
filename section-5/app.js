cfbe1bd23cf706cf098fee1551250f6cc2c26569
m-swelam <mid2o0o11@gmail.com>
2021-11-09 13:30:12 +0200

    section 5 - 30

--- section-5/app.js
+++ section-5/app.js
@@ -4,15 +4,11 @@ const app = express();
 const port = process.env.PORT || 9999;
 
 app.get("/", (req, res) => {
-  res.send("<h1>hello world!");
+  res.send('<h1>home</h1>')
 });
 
-app.get("/api", (req, res) => {
-  res.send("<h1>API</h1>");
-});
-
-app.get("/json", (req, res) => {
-  res.json({name:'swelam'})
+app.get("/post/:id", (req, res) => {
+  res.send(`<h1>post id is : ${req.params.id}`);
 });
 
 app.listen(port);
