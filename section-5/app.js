2b0898bc4dbf805989b82b9849ebb6f69374536a
m-swelam <mid2o0o11@gmail.com>
2021-11-09 14:54:36 +0200

    section 5 - 32

--- section-5/app.js
+++ section-5/app.js
@@ -1,30 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 const port = process.env.PORT || 9999;
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    <h1>home</h1>
-  </body>
-  </html>
-  `);
+  res.sendFile("index.html");
 });
 
-app.get("/post/:id", (req, res) => {
-  res.send(`<h1>post id is : ${req.params.id}`);
+app.post("/post", (req, res) => {
+  console.log(`user email ${req.body.email} and password is ${req.body.password}`);
 });
 
 app.listen(port);
