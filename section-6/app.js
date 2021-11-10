305e5efa1542b245bad4767e5f4c748fe0e17d5a
m-swelam <mid2o0o11@gmail.com>
2021-11-10 11:28:51 +0200

    section-6 34

--- section-6/app.js
+++ section-6/app.js
@@ -2,5 +2,7 @@ const WebSocketServer = require("ws").Server;
 const WSS = new WebSocketServer({ port: 3232 });
 
 WSS.on("connection", (ws) => {
-  console.log("we are connected");
+  ws.on("message", (message) => {
+    console.log(message.toString());
+  });
 });
